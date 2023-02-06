25d2a0a29e154a5770390a4235b62fce04b5b043
Josh <111532201+Joshtdale@users.noreply.github.com>
2023-02-06 12:08:02 -0500

    Update sandbox.cs

--- sandbox.cs
+++ sandbox.cs
@@ -1,1 +1,18 @@
+using System;
 
+namespace my_sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                Console.WriteLine("What is your name?");
+
+                string userName = Console.ReadLine();
+
+                Console.WriteLine("Hello " + userName + ", nice to meet you.")
+
+                Console.ReadKey();
+            }
+    }
+}
